510d68adb2d5479cfab46f4f843651f60189ff36
Hugues2601 <huguesrdc@gmail.com>
2024-12-11 22:41:32 +0100

    v1

--- HestonModel/ForwardStart.py
+++ HestonModel/ForwardStart.py
@@ -1,6 +1,27 @@
 import torch
 from config import CONFIG
 
+class ForwardStart:
+    def __init__(self, S0, k, T0, T1, T2, r, kappa, v0, theta, sigma, rho):
+        self.S0 = S0
+        self.k = k
+        self.T0 = T0
+        self.T1 = T1
+        self.T2 = T2
+        self.r = r
+        self.kappa = kappa
+        self.v0 = v0
+        self.theta = theta
+        self.sigma = sigma
+        self.rho = rho
+
+
+
+
+
+
+
+
 def fs_heston_cf(phi, S0, T0, T1, T2, r, kappa, v0, theta, sigma, rho):
     # Ensure that phi is a torch tensor on the GPU
     if not isinstance(phi, torch.Tensor):
